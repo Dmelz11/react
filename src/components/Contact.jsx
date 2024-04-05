9f4aa79b45e3e19ea60260d825c6cb60ede6bbba
davidmelendez <Dmelz11@github.com>
2024-04-05 10:13:45 -0700

    added emailjs as dependency

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import '../styles/Contact.css';
 
 //  importing a helper function that will check if the email is valid
@@ -7,6 +9,7 @@ import { validateEmail } from "../../utils/helpers";
 function ContactForm() {
   // Create state variables for the fields in the form
   // We are also setting their initial values to an empty string
+  const form = useRef();
   const [formState, setFormState] = useState({ name: '', email: '', message: ''});
   const {name, email, message} = formState;
   const [errorMessage, setErrorMessage] = useState('');
@@ -33,25 +36,35 @@ function ContactForm() {
      }
      console.log('errorMessage', errorMessage);
     }
-  
+    const sendEmail = (e) => {
+      e.preventDefault();
 
-  const handleSubmit = (e) => {
-    // This prevents the default behavior of the form
-    e.preventDefault();
-    console.log(formState);
-  
-  }
+      emailjs
+      .sendForm('service_o1xdypg', 'template_v5bgfui', form.current, {
+        publicKey: 'bD5rjFJ-jApY5ZInA',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+ 
   return (
     <form>
       <div className="page-body">
         <h1 className="heading">Let's Connect</h1>
 
-        <form className="form" onSubmit={handleSubmit}>
+        <form ref={form} onSubmit={sendEmail}>
           <div>
             <div class="form-group">
               <label htmlFor="name" className="contact_form-tag">Name</label>
               <input
                 type="text"
+                name="user_name"
                 className="form-control"
                 defaultValue={name}
                 onBlur={handleChange}
@@ -64,6 +77,7 @@ function ContactForm() {
             <label htmlFor="email" className="contact_form-tag">Email</label>
             <input
               type="email"
+              name="user_email"
               className="form-control"
               defaultValue={email}
               onBlur={handleChange}
@@ -74,6 +88,7 @@ function ContactForm() {
           <div class="form-group">
             <label htmlFor="message" className="contact_form-tag">Message</label>
             <textarea
+               type="submit"
                name="message"
                className="form-control"
                rows="5"
@@ -87,17 +102,15 @@ function ContactForm() {
           </div>
         )}
           <br></br>
-         
-          <button type="submit" className="bg btn-primary ">         
-            Submit
+          <div className='contactContainer'>
+          <button type="submit" className="bg btn-primary">         
+            Send
           </button>
+          </div>
         </form>
        
       </div>
-      <br></br>
-      <div className='contactContainer'>
-      <p>Contact Me:<a href="mailto: david@greensprings.us" className="contact_button">david@greensprings.us</a></p>
-      </div>
+    
     </form>
   );
           }      
